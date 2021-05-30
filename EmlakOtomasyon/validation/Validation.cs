46c7abe7f74304d5aeb71c492c3d03a89f141995
Erhan BİÇER <iletisim@erhanbicer.com>
2021-05-30 16:32:02 +0300

    Validasyon helper oluşturuldu

---
+++ EmlakOtomasyon/validation/Validation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EmlakOtomasyon.validation
+{
+    class Validation
+    {
+        public static bool kullaniciAdi(string kullaniciAdi)
+        {
+            bool isValid = kullaniciAdi.Length < 3;
+            if (isValid)
+            {
+                MessageBox.Show("Kullanici adi 3 karakterden küçük olamaz", "Kullanici Adi");
+            }
+
+            return isValid;
+        }
+
+        public static bool sifre(string sifre)
+        {
+            bool isValid = sifre.Length < 3;
+            if (isValid)
+            {
+                MessageBox.Show("Şifre 6 karakterden küçük olamaz", "Şifre");
+            }
+
+            return isValid;
+        }
+    }
+}
